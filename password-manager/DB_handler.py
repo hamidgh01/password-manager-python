35d4264a8a7410d140cd8d3b7834df725e6bbf4e
Hamid <hamidghahremani2001@gmail.com>
2025-02-10 19:11:23 +0330

    organize DB_handler.py basic structure

--- password-manager/DB_handler.py
+++ password-manager/DB_handler.py
@@ -1,21 +1,50 @@
 """
-a simple ORM, specially was designed & is utilized for
-this project (password generator and manager)
+database handler, has been specially designed and is specially utilized
+for this project (password generator and manager using python)
 """
 
 import sqlite3
 
 
-class DB:
-    """orm"""
+class DBHandler:
+    """database handler for the app"""
     
-    def __init__(self, database="database"):
+    # superuser (admin)
+    # current_user (logged-in user)
+    
+    def __init__(self, database: str = "database"):
+        if database == ":memory:":
+            pass
+        elif not database.endswith(".db"):
+            database += ".db"
+        
         self.database = database
     
     def __enter__(self):
         self.connector = sqlite3.connect(database=self.database)
         self.db_cursor = self.connector.cursor()
-        self._create_table()
+        
+        create_table_users = """
+            CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT(64) NOT NULL UNIQUE,
+                password TEXT(32) NOT NULL
+            );
+        """
+        create_table_passwords = """
+            CREATE TABLE IF NOT EXISTS passwords(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                service_name TEXT(64) DEFAULT "NOT PROVIDED",
+                username TEXT(64) DEFAULT "NOT PROVIDED",
+                password TEXT(32) NOT NULL,
+                user_id INTEGER NOT NULL,
+                FOREIGN KEY(user_id) REFERENCES users(id)
+            );
+            """
+        
+        self.db_cursor.execute(create_table_users)
+        self.db_cursor.execute(create_table_passwords)
+        self.connector.commit()
     
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.connector.close()
@@ -36,22 +65,21 @@ class DB:
         """delete"""
         pass
     
-    def create_table(self):
-        """create_table"""
+    def register_user(self):
+        """register new user"""
         pass
     
-    def create_table(self):
-        """create_table"""
+    def change_user_password(self):
+        """change user password"""
         pass
     
-    def create_table(self):
-        """create_table"""
-        pass
+
+def test():
+    """complete here later"""
     
-    def create_table(self):
-        """create_table"""
+    with DBHandler("my_database"):
         pass
-    
-    def create_table(self):
-        """create_table"""
-        pass
+
+
+if __name__ == '__main__':
+    test()
